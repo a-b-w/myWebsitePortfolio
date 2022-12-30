3d6d1f73aa22d84185d4d4e4a69669d600858059
a-b-w <alexanderblakewest@gmail.com>
2022-12-30 17:52:33 -0600

    playing with responsivness resized mugshot

---
+++ src/app.js
@@ -0,0 +1,6 @@
+/*const hamburgerButton = document.getElementById('hamburger')
+const navList = document.getElementById('nav-list')
+
+function toggle:() {
+    navList.classList.toggle('show')
+}*/
